51e31302266b4c7f6f89a1cdcbd65c04c11b536a
SkyHunter <ledemas09@gmail.com>
2025-08-12 09:38:18 +0100

    fetching the server

---
+++ MySQLServer.py
@@ -0,0 +1,24 @@
+# Build my Database: my Gateway to Data Adventure!
+import mysql.connector
+from mysql.connector import Error
+try:
+    #connecting to my sql server
+    connection = mysql.connector.connect(
+        host = "localhost",
+        user = "root",
+        password = "FanyaKazi{30}"
+    )
+    if connection.is_connected():
+        cursor = connection.cursor()
+        # create database if doesnt exist
+        cursor.execute("CREATE DATABASE IF NOT EXISTS alx_book_store")
+        print("Database 'alx_book_store' created successfully!")
+except Error as err:
+    print(f"Error: {err}")
+finally:
+    if 'cursor' in locals():
+        cursor.close()
+    if 'connection' in locals() and connection.is_connected():
+        connection.close()
+
+
