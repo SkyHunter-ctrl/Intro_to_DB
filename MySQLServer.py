7acc94cf92e6a8135aec2e8e68f4c6ff7184ab62
SkyHunter <ledemas09@gmail.com>
2025-08-12 09:52:50 +0100

    fetching the server

--- MySQLServer.py
+++ MySQLServer.py
@@ -13,7 +13,7 @@ try:
         # create database if doesnt exist
         cursor.execute("CREATE DATABASE IF NOT EXISTS alx_book_store")
         print("Database 'alx_book_store' created successfully!")
-except Error as err:
+except mysql.connector.Error as err:
     print(f"Error: {err}")
 finally:
     if 'cursor' in locals():
