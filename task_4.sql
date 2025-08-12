f19bee132dcd0fad76f12e75f566e484a1eece06
SkyHunter <ledemas09@gmail.com>
2025-08-12 13:24:08 +0100

    print columns of book table

--- task_4.sql
+++ task_4.sql
@@ -4,3 +4,5 @@ USE alx_book_store;
 # You are not allowed to use the DESCRIBE or EXPLAIN statements
 SHOW CREATE TABLE Books;
 # This will show the full structure of the Books table including columns, types, and constraints.
+SHOW COLUMNS FROM Books;
+# This will also provide detailed information about each column in the Books table.
