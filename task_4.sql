ab1c2969ccd589da617e1459937890b52578347f
SkyHunter <ledemas09@gmail.com>
2025-08-12 13:20:02 +0100

    print columns of book table

---
+++ task_4.sql
@@ -0,0 +1,6 @@
+# a script that prints the full description of the table books from the database alx_book_store in your MySQL server.
+USE alx_book_store;
+# print the full description of the table books
+# You are not allowed to use the DESCRIBE or EXPLAIN statements
+SHOW CREATE TABLE Books;
+# This will show the full structure of the Books table including columns, types, and constraints.
