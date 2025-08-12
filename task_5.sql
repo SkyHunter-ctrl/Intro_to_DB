764d87d732a3415187cde3664fd459ee15e83d7c
SkyHunter <ledemas09@gmail.com>
2025-08-12 13:57:25 +0100

    inserting customers details

--- task_5.sql
+++ task_5.sql
@@ -1,6 +1,6 @@
 # a script that inserts a single row in the table customer (database alx_book_store) in your MySQL server.
 USE alx_book_store;
-INSERT INTO Customers (customer_id, customer_name, email, address);
+INSERT INTO customers (customer_id, customer_name, email, address);
 VALUES
 (1,
 'Cole Baidoo',
